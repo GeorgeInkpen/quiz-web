97e0c81660f64a2a8bf9b6fb68a86e56ac5e6666
Georgina Inkpen <116553390+GeorgeInkpen@users.noreply.github.com>
2024-04-03 13:43:52 +0100

    Update config.service.js

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quiz-api-gi3-env.eba-dfunr3qb.us-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'http://localhost:8000/';
 
     return service;
 }]);
